9c67dc3c201343311a9cffbca5e904cf4f588801
Andy Brodie <andy@locima.com>
2012-10-22 18:37:39 +0100

    Added lots more comments

--- Locima.SlidingBlock/IO/IsolatedStorage/IOHelper.cs
+++ Locima.SlidingBlock/IO/IsolatedStorage/IOHelper.cs
@@ -10,20 +10,48 @@ using NLog;
 
 namespace Locima.SlidingBlock.IO.IsolatedStorage
 {
+    /// <summary>
+    ///   Various helper utility for IO operations on Isolated Storage (the local storage area on the phone)
+    /// </summary>
     public class IOHelper
     {
-        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+        /// <summary>
+        ///   The strings that separates directories within a path
+        /// </summary>
         public const string PathSeparator = "\\";
 
-        public void DownloadStream(Uri uri, string isolatedStoragePath)
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
+        /// <summary>
+        ///   Downloads the file located at <paramref name="uri" /> and saves it to the <paramref name="isolatedStoragePath" />.  Any existing file is overwritten.
+        /// </summary>
+        /// <remarks>
+        ///   Delegates to <see cref="DownloadStream(System.Uri,Stream)" />
+        /// </remarks>
+        /// <param name="uri"> Download source, must not be null </param>
+        /// <param name="isolatedStoragePath"> Download destination, must not be null </param>
+        /// <returns> Response from <see cref="DownloadStream(System.Uri,Stream)" /> </returns>
+        public IAsyncResult DownloadStream(Uri uri, string isolatedStoragePath)
         {
+            if (uri == null) throw new ArgumentNullException("uri");
+            if (string.IsNullOrEmpty(isolatedStoragePath)) throw new ArgumentNullException("isolatedStoragePath");
             IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication();
             IsolatedStorageFileStream streamToWriteTo = new IsolatedStorageFileStream(isolatedStoragePath,
                                                                                       FileMode.Create, store);
-            DownloadStream(uri, streamToWriteTo);
+            return DownloadStream(uri, streamToWriteTo);
         }
 
 
+        /// <summary>
+        ///   Downloads the stream from <paramref name="uri" /> and writes it to <paramref name="streamToWriteTo" />
+        /// </summary>
+        /// <remarks>
+        ///   Uses <see cref="HttpWebRequest" /> to download the file.  See <see cref="DownloadStreamWorker" />.  For parameters of download, see <see
+        ///    cref="DownloadRequest" />
+        /// </remarks>
+        /// <param name="uri"> Download source, must not be null </param>
+        /// <param name="streamToWriteTo"> The stream to write the downloaded data to, must not be null </param>
+        /// <returns> Response object from <see cref="HttpWebRequest.BeginGetResponse" /> </returns>
         public static IAsyncResult DownloadStream(Uri uri, Stream streamToWriteTo)
         {
             HttpWebRequest request = (HttpWebRequest) WebRequest.Create(uri);
@@ -33,15 +61,25 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
                                                                {
                                                                    SourceUri = uri,
                                                                    Request = request,
-                                                                   OutputStream = streamToWriteTo
+                                                                   OutputStream = streamToWriteTo,
+                                                                   Buffersize = 256,
+                                                                   Callback =
+                                                                       (dlReq, totalDownloaded, totalSize) =>
+                                                                       Logger.Debug(
+                                                                           "Downloaded {0} / {1} bytes of {2}",
+                                                                           totalDownloaded, totalSize, dlReq.SourceUri)
                                                                });
             return result;
         }
 
 
+        /// <summary>
+        ///   Used with <see cref="DownloadStream(System.Uri,Stream)" /> to download a file to isolated storage, using <see
+        ///    cref="HttpWebRequest.BeginGetResponse" />
+        /// </summary>
+        /// <param name="result"> The object encapsulating the request to download </param>
         private static void DownloadStreamWorker(IAsyncResult result)
         {
-            // TODO Tidy this up, allow callbacks on incremental increases in download progress
             DownloadRequest req = (DownloadRequest) result.AsyncState;
             HttpWebRequest request = req.Request;
             Stream streamToWriteTo = req.OutputStream;
@@ -50,15 +88,18 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
                 HttpWebResponse response = (HttpWebResponse) request.EndGetResponse(result);
                 Stream inputStream = response.GetResponseStream();
 
-                byte[] data = new byte[256*1024];
+                byte[] data = new byte[req.Buffersize*1024];
                 int bytesRead;
 
                 long totalValue = response.ContentLength;
+                long totalBytesRead = 0;
                 while ((bytesRead = inputStream.Read(data, 0, data.Length)) > 0)
                 {
+                    totalBytesRead += bytesRead;
                     if (streamToWriteTo.Length != 0)
                         Logger.Info((int) ((streamToWriteTo.Length*100)/totalValue));
                     streamToWriteTo.Write(data, 0, bytesRead);
+                    if (req.Callback != null) req.Callback(req, totalBytesRead, totalValue);
                 }
             }
             finally
@@ -69,6 +110,10 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
         }
 
 
+        /// <summary>
+        /// </summary>
+        /// <param name="filename"> </param>
+        /// <param name="stream"> </param>
         public void SaveStream(string filename, Stream stream)
         {
             using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
@@ -85,10 +130,15 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
             }
         }
 
+        /// <summary>
+        ///   Copies the data from <paramref name="inStream" /> to <see cref="outStream" />
+        /// </summary>
+        /// <param name="inStream"> The stream to copy data from </param>
+        /// <param name="outStream"> The stream to copy data to </param>
         public static void CopyStream(Stream inStream, Stream outStream)
         {
             int count;
-            byte[] buffer = new byte[512000];
+            byte[] buffer = new byte[512*1024];
             do
             {
                 count = inStream.Read(buffer, 0, buffer.Length);
@@ -96,25 +146,12 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
             } while (count > 0);
         }
 
-
-        #region Nested type: DownloadRequest
-
-        private struct DownloadRequest
-        {
-            public Uri SourceUri { get; set; }
-            public HttpWebRequest Request { get; set; }
-            public Stream OutputStream { get; set; }
-        }
-
-        #endregion
-
-
         /// <summary>
-        /// Creates the directory named within isolated storage
+        ///   Creates the directory named within isolated storage
         /// </summary>
-        /// <param name="directoryName">The name of the directory to create, must be a valid directory name</param>
-        /// <param name="store"></param>
-        /// <returns>True if the directory was created, false if it already existed</returns>
+        /// <param name="directoryName"> The name of the directory to create, must be a valid directory name </param>
+        /// <param name="store"> </param>
+        /// <returns> True if the directory was created, false if it already existed </returns>
         public static bool EnsureDirectory(string directoryName, IsolatedStorageFile store)
         {
             bool created;
@@ -134,10 +171,10 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
 
 
         /// <summary>
-        /// Creates a directory if it doesn't exist
+        ///   Creates a directory if it doesn't exist
         /// </summary>
-        /// <param name="directoryName">The name of the directory to create</param>
-        /// <returns><c>true</c> is the directory had to be created, <c>false</c> if it already existed</returns>
+        /// <param name="directoryName"> The name of the directory to create </param>
+        /// <returns> <c>true</c> is the directory had to be created, <c>false</c> if it already existed </returns>
         public static bool EnsureDirectory(string directoryName)
         {
             using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
@@ -148,11 +185,11 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
 
 
         /// <summary>
-        /// Returns the full path names of files within the direcctory specified by <paramref name="directoryName"/>
+        ///   Returns the full path names of files within the direcctory specified by <paramref name="directoryName" />
         /// </summary>
-        /// <param name="directoryName">The name of the directory to search within</param>
-        /// <param name="store">The store to search</param>
-        /// <returns>A (potentially empty) list of files</returns>
+        /// <param name="directoryName"> The name of the directory to search within </param>
+        /// <param name="store"> The store to search </param>
+        /// <returns> A (potentially empty) list of files </returns>
         public static List<string> GetFileNames(string directoryName, IsolatedStorageFile store)
         {
             string[] fileNames = store.GetFileNames(string.Format("{0}{1}*", directoryName, PathSeparator));
@@ -162,29 +199,28 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
             return fullFileNames;
         }
 
-                /// <summary>
-        /// Saves an object to isolated storage, creating or overwriting if necessary, using the default <see cref="DataContractSerializer"/>
+        /// <summary>
+        ///   Saves an object to isolated storage, creating or overwriting if necessary, using the default <see
+        ///    cref="DataContractSerializer" />
         /// </summary>
-        /// <typeparam name="T">The type of object to be saved</typeparam>
-        /// <param name="obj">The object to save</param>
+        /// <typeparam name="T"> The type of object to be saved </typeparam>
+        /// <param name="obj"> The object to save </param>
         public static void SaveObject<T>(T obj) where T : class, IPersistedObject
-                {
-                    using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
-                    {
-                        SaveObject(obj, store);
-                    }
-                }
-
-
-
+        {
+            using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
+            {
+                SaveObject(obj, store);
+            }
+        }
 
 
         /// <summary>
-        /// Saves an object to isolated storage, creating or overwriting if necessary, using the default <see cref="DataContractSerializer"/>
+        ///   Saves an object to isolated storage, creating or overwriting if necessary, using the default <see
+        ///    cref="DataContractSerializer" />
         /// </summary>
-        /// <typeparam name="T">The type of object to be saved</typeparam>
-        /// <param name="obj">The object to save</param>
-        /// <param name="store">The store to save data to</param>
+        /// <typeparam name="T"> The type of object to be saved </typeparam>
+        /// <param name="obj"> The object to save </param>
+        /// <param name="store"> The store to save data to </param>
         public static void SaveObject<T>(T obj, IsolatedStorageFile store) where T : class, IPersistedObject
         {
             if (obj == null) throw new ArgumentException("obj");
@@ -211,18 +247,20 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
 
 
         /// <summary>
-        /// Loads an object from Isolated Storage
+        ///   Loads an object from Isolated Storage
         /// </summary>
         /// <remarks>
-        /// Uses <See cref="DataContractSerializer"/> to load data from isolated storage and sets <see cref="IPersistedObject.LastUpdate"/> 
-        /// and <see cref="IPersistedObject.Id"/> to <paramref name="filename"/></remarks>
-        /// <typeparam name="T">The type of object to deserialise to</typeparam>
-        /// <param name="store">The isolated storage file to read from</param>
-        /// <param name="filename">The filename to load from (must not be null)</param>
-        /// <returns>The loaded object</returns>
+        ///   Uses <See cref="DataContractSerializer" /> to load data from isolated storage and sets <see
+        ///    cref="IPersistedObject.LastUpdate" /> 
+        ///   and <see cref="IPersistedObject.Id" /> to <paramref name="filename" />
+        /// </remarks>
+        /// <typeparam name="T"> The type of object to deserialise to </typeparam>
+        /// <param name="store"> The isolated storage file to read from </param>
+        /// <param name="filename"> The filename to load from (must not be null) </param>
+        /// <returns> The loaded object </returns>
         public static T LoadObject<T>(IsolatedStorageFile store, string filename) where T : class, IPersistedObject
         {
-            Logger.Info("Loading file {0} and deserialising to instance of {1}", filename, typeof(T).FullName);
+            Logger.Info("Loading file {0} and deserialising to instance of {1}", filename, typeof (T).FullName);
 
             if (store == null) throw new ArgumentNullException("store");
             if (string.IsNullOrEmpty(filename)) throw new ArgumentNullException("filename");
@@ -231,15 +269,18 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
                 throw new FileNotFoundException(filename);
             }
 
-            using (IsolatedStorageFileStream fileStream = store.OpenFile(filename, FileMode.Open, FileAccess.Read, FileShare.None))
+            using (
+                IsolatedStorageFileStream fileStream = store.OpenFile(filename, FileMode.Open, FileAccess.Read,
+                                                                      FileShare.None))
             {
-                DataContractSerializer serializer = new DataContractSerializer(typeof(T));
-                T deserialisedObject = (T)serializer.ReadObject(fileStream);
+                DataContractSerializer serializer = new DataContractSerializer(typeof (T));
+                T deserialisedObject = (T) serializer.ReadObject(fileStream);
 
                 // Set the non-persisted filename property
                 deserialisedObject.Id = filename;
                 deserialisedObject.LastUpdate = store.GetLastWriteTime(filename);
-                Logger.Info("Successfully read {0}, deserialised to {1} and set last update {2}", filename, deserialisedObject, deserialisedObject.LastUpdate);
+                Logger.Info("Successfully read {0}, deserialised to {1} and set last update {2}", filename,
+                            deserialisedObject, deserialisedObject.LastUpdate);
 
                 return deserialisedObject;
             }
@@ -247,11 +288,11 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
 
 
         /// <summary>
-        /// Loads an object from Isolated Storage
+        ///   Loads an object from Isolated Storage
         /// </summary>
-        /// <typeparam name="T"></typeparam>
-        /// <param name="filename">The filename to load from (must not be null)</param>
-        /// <returns>The loaded object</returns>
+        /// <typeparam name="T"> </typeparam>
+        /// <param name="filename"> The filename to load from (must not be null) </param>
+        /// <returns> The loaded object </returns>
         public static T LoadObject<T>(string filename) where T : class, IPersistedObject
         {
             using (IsolatedStorageFile store = IsolatedStorageFile.GetUserStoreForApplication())
@@ -261,6 +302,11 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
         }
 
 
+        /// <summary>
+        /// Deletes a file, delegates to <see cref="DeleteFile(string, bool)"/> passing <c>false</c> as the <c>suppressException</c> parameter
+        /// </summary>
+        /// <param name="filename">The name the file to delete</param>
+        /// <returns><c>True</c> if the file was found and deleted, <c>false</c> otherwise</returns>
         public static bool DeleteFile(string filename)
         {
             return DeleteFile(filename, false);
@@ -268,11 +314,11 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
 
 
         /// <summary>
-        /// Deletes a file from Isolated Storage
+        ///   Deletes a file from Isolated Storage
         /// </summary>
-        /// <param name="filename">The name of the file to delete</param>
-        /// <param name="suppressException">If true, all exceptions will be swallowed (but logged)</param>
-        /// <returns><c>true</c> if the file was successfully deleted, <c>false</c> otherwise</returns>
+        /// <param name="filename"> The name of the file to delete </param>
+        /// <param name="suppressException"> If true, all exceptions will be swallowed (but logged) </param>
+        /// <returns> <c>true</c> if the file was successfully deleted, <c>false</c> otherwise </returns>
         public static bool DeleteFile(string filename, bool suppressException)
         {
             bool result;
@@ -311,5 +357,41 @@ namespace Locima.SlidingBlock.IO.IsolatedStorage
             return result;
         }
 
+        #region Nested type: DownloadRequest
+
+        /// <summary>
+        ///   Represents a download request, used with <see cref="IOHelper.DownloadStream(System.Uri,string)" />, <see
+        ///    cref="IOHelper.DownloadStream(System.Uri,String)" />
+        ///   and <see cref="IOHelper.DownloadStreamWorker" />
+        /// </summary>
+        public struct DownloadRequest
+        {
+            /// <summary>
+            ///   The Uri to download data from
+            /// </summary>
+            public Uri SourceUri { get; set; }
+
+            /// <summary>
+            ///   The request object that's doing the downloading
+            /// </summary>
+            public HttpWebRequest Request { get; set; }
+
+            /// <summary>
+            ///   Where to write the data to
+            /// </summary>
+            public Stream OutputStream { get; set; }
+
+            /// <summary>
+            ///   Data will be downloading chunks of this size
+            /// </summary>
+            public int Buffersize { get; set; }
+
+            /// <summary>
+            ///   Callback after each chunk is downloaded.  Second parameter is the number of bytes downloaded so far, third is the total number of bytes expected.
+            /// </summary>
+            public Action<DownloadRequest, long, long> Callback { get; set; }
+        }
+
+        #endregion
     }
 }
