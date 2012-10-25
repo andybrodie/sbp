bca2a3ad614c69888e3e1ab066de921372076bd4
Andy Brodie <andy@locima.co.uk>
2012-10-25 08:04:58 +0100

    Improved commentary

--- Locima.SlidingBlock/GameTemplates/GameDefinition.cs
+++ Locima.SlidingBlock/GameTemplates/GameDefinition.cs
@@ -3,9 +3,16 @@ using System.Runtime.Serialization;
 
 namespace Locima.SlidingBlock.GameTemplates
 {
+
+    /// <summary>
+    /// Used to define a game, which consists of a sequence of level (<see cref="LevelDefinition"/> instances)
+    /// </summary>
     [DataContract]
     public class GameDefinition
     {
+        /// <summary>
+        /// A sequence of levels that make up the game
+        /// </summary>
         [DataMember]
         public List<LevelDefinition> Levels { get; set; }
         
--- Locima.SlidingBlock/GameTemplates/LicenseDefinition.cs
+++ Locima.SlidingBlock/GameTemplates/LicenseDefinition.cs
@@ -3,12 +3,22 @@ using System.Runtime.Serialization;
 
 namespace Locima.SlidingBlock.GameTemplates
 {
+
+    /// <summary>
+    /// Used to model the license applied to a <see cref="LevelDefinition"/>
+    /// </summary>
     [DataContract]
     public class LicenseDefinition
     {
+        /// <summary>
+        /// A link to the full definition/text of the license
+        /// </summary>
         [DataMember]
         public Uri Link { get; set; }
 
+        /// <summary>
+        /// The title of the license
+        /// </summary>
         [DataMember]
         public string Title { get; set; }
     }
