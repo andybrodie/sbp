1008f296321761ce974b8a5be00192e307fcc3d0
Andy Brodie <andy@locima.co.uk>
2012-10-30 17:24:32 +0000

    Fixed bug that the wrong property name was raised in the menu item view model for Text and Image, also added commentary

--- Locima.SlidingBlock/ViewModel/HighScoresViewModel.cs
+++ Locima.SlidingBlock/ViewModel/HighScoresViewModel.cs
@@ -8,22 +8,57 @@ using Locima.SlidingBlock.Persistence;
 
 namespace Locima.SlidingBlock.ViewModel
 {
+
+    /// <summary>
+    /// View model for the <see cref="HighScores"/> page using <see cref="HighScoreTable"/> as the model.  This class also uses <see cref="HighScoreItemViewModel"/> for individual
+    /// entries within the high score table
+    /// </summary>
     public class HighScoresViewModel : ViewModelBase
     {
+
+        /// <summary>
+        /// The name of the ListBox control that contains the high score entries.
+        /// </summary>
+        /// <remarks>
+        /// This is used to allow the <see cref="ScrollToViewMessage"/> event handler to identify the ListBox to scroll to</remarks>
         public const string HighScoreTableViewObjectName = "HighScoreTableControl";
+
+        /// <summary>
+        /// Backing field for <see cref="ButtonLabel"/>
+        /// </summary>
         private string _buttonLabel;
+
+        /// <summary>
+        /// Backing field for <see cref="Model"/>
+        /// </summary>
         private HighScoreTable _model;
 
+
+        /// <summary>
+        /// Initialises <see cref="HighScores"/> with an empty collection and <see cref="ClickCommand"/>
+        /// </summary>
         public HighScoresViewModel()
         {
             HighScores = new ObservableCollection<HighScoreItemViewModel>();
             ClickCommand = new DelegateCommand(HandleClick);
         }
 
+        /// <summary>
+        /// The list of all the high scores
+        /// </summary>
+        /// <remarks>
+        /// This is the list of items to display in the high score table
+        /// </remarks>
         public ObservableCollection<HighScoreItemViewModel> HighScores { get; private set; }
 
+        /// <summary>
+        /// 
+        /// </summary>
         public ICommand ClickCommand { get; set; }
 
+        /// <summary>
+        /// The label for the button that allows the user to navigate to the "next" page after the high score screen.  This changes depending on how the high score table page was arrived at
+        /// </summary>
         public string ButtonLabel
         {
             get { return _buttonLabel; }
@@ -34,18 +69,20 @@ namespace Locima.SlidingBlock.ViewModel
             }
         }
 
+        /// <summary>
+        /// The Uri to navigate to when the user clicks the button at the bottom of the high score page.  If null then we will navigate back.
+        /// </summary>
         public Uri ButtonUri { get; set; }
 
+        /// <summary>
+        /// Requests the view sends us to the next page.  What the next page is depends on the value of <see cref="ButtonUri"/>
+        /// </summary>
+        /// <param name="obj"></param>
         private void HandleClick(object obj)
         {
-            if (null==ButtonUri)
-            {
-                SendViewMessage(new NavigationMessageArgs {NavigationMode = NavigationMode.Back});
-            }
-            else
-            {
-                SendViewMessage(new NavigationMessageArgs(ButtonUri));
-            }
+            SendViewMessage(null == ButtonUri
+                                ? new NavigationMessageArgs {NavigationMode = NavigationMode.Back}
+                                : new NavigationMessageArgs(ButtonUri));
         }
 
         /// <summary>
@@ -61,6 +98,9 @@ namespace Locima.SlidingBlock.ViewModel
         }
 
 
+        /// <summary>
+        /// Wipe the <see cref="HighScores"/> view model collection and reload it from the <see cref="Model"/> and sort by high score
+        /// </summary>
         private void ResyncViewModelFromModel()
         {
             HighScores.Clear();
