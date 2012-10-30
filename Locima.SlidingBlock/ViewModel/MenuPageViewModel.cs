b281d7ed36e063980fbd9de761140d893a77dfed
Andy Brodie <andy@locima.co.uk>
2012-10-30 17:39:42 +0000

    Added commentary

--- Locima.SlidingBlock/ViewModel/MenuPageViewModel.cs
+++ Locima.SlidingBlock/ViewModel/MenuPageViewModel.cs
@@ -3,11 +3,25 @@ using System.Collections.Specialized;
 
 namespace Locima.SlidingBlock.ViewModel
 {
+    /// <summary>
+    /// The view model for a single menu page, consisting of just a title and a collection of view models (<see cref="MenuItemViewModel"/>) for each menu item
+    /// </summary>
     public class MenuPageViewModel : ViewModelBase
     {
+
+        /// <summary>
+        /// Backing field for <see cref="MenuItems"/>
+        /// </summary>
         private ObservableCollection<MenuItemViewModel> _menuItems;
+
+        /// <summary>
+        /// Backing field for <see cref="PageTitle"/>
+        /// </summary>
         private string _pageTitle;
 
+        /// <summary>
+        /// The title of the menu page
+        /// </summary>
         public string PageTitle
         {
             get { return _pageTitle; }
@@ -18,6 +32,9 @@ namespace Locima.SlidingBlock.ViewModel
             }
         }
 
+        /// <summary>
+        /// The list of view models for menu items to be shown on the menu page
+        /// </summary>
         public ObservableCollection<MenuItemViewModel> MenuItems
         {
             get { return _menuItems; }
@@ -35,6 +52,9 @@ namespace Locima.SlidingBlock.ViewModel
             }
         }
 
+        /// <summary>
+        /// Raised when a new menu item is added or an existing one removed from <see cref="MenuItems"/>
+        /// </summary>
         private void MenuItemsOnCollectionChanged(object sender,
                                                   NotifyCollectionChangedEventArgs notifyCollectionChangedEventArgs)
         {
