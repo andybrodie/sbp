ead10e58e21e4bf08b5e3c891d9cfbac943d7b81
Andy Brodie <andy@locima.com>
2013-03-11 07:58:38 +0000

    Added logging, commentary and renamed variables to be more explanatory

--- Locima.SlidingBlock/Common/BubbleSorter.cs
+++ Locima.SlidingBlock/Common/BubbleSorter.cs
@@ -1,5 +1,7 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
 using Locima.SlidingBlock.Scrambles;
+using NLog;
 
 namespace Locima.SlidingBlock.Common
 {
@@ -11,6 +13,8 @@ namespace Locima.SlidingBlock.Common
     /// <para>We need our own implementation because we need to know how many swaps were required to sort the list.</para></remarks>
     public class BubbleSorter
     {
+        private static readonly Logger Logger = LogManager.GetCurrentClassLogger();
+
         /// <summary>
         /// Simple optimised bubble sorter that uses the default <see cref="Comparer{T}"/> implementation for <typeparamref name="T"/>
         /// </summary>
@@ -36,24 +40,28 @@ namespace Locima.SlidingBlock.Common
         public static IList<T> Sort<T>(IList<T> list, IComparer<T> comparer, out int swapCount)
         {
             IList<T> workList = new List<T>(list);
+            Logger.Info(String.Join(",", workList));
             int swapCounter = 0;
-            bool incomplete = true;
-            while (incomplete)
+            bool anotherPassRequired = true;
+            while (anotherPassRequired)
             {
-                incomplete = false;
-                for (int i = 0; i < workList.Count - 1; i++)
+                anotherPassRequired = false;
+                for (int index = 0; index < workList.Count - 1; index++)
                 {
-                    T x = workList[i];
-                    T y = workList[i + 1];
-                    if (comparer.Compare(x, y) > 0)
+                    T first = workList[index];
+                    T second = workList[index + 1];
+                    if (comparer.Compare(first, second) > 0)
                     {
                         // If the two elements in in the wrong order, swap them
-                        workList[i] = y;
-                        workList[i + 1] = x;
+                        workList[index] = second;
+                        workList[index + 1] = first;
 
                         // Keep track of how many swaps its taking to solve the puzzle
                         swapCounter++;
-                        incomplete = true;
+                        /* As we've made a change during this pass we need to make a subsequent pass 
+                         * (only when a complete pass is made with no changes are we sure we're sorted, as it were) */
+                        anotherPassRequired = true;
+                        Logger.Info(String.Join(",", workList));
                     }
                 }
                 // If we've made a pass through the entire list and made no swaps, then the list is sorted, incomplete will remain false and cause the loop to exit
